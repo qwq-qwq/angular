ddc9d07fe80b5c845baf1b5f62bb359abd91b8bc
IT-Discovery <sergey.morenets@gmail.com>
2017-06-10 17:08:50 +0300

    Generating prime numbers

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -4,6 +4,7 @@ import {CurrencyPipe} from '@angular/common';
 import {BookService} from '../book.service';
 import {Observable} from 'rxjs/Rx';
 import {Http} from '@angular/http';
+import {Observer} from "rxjs/Observer";
 
 @Component({
   selector: 'app-books',
@@ -19,16 +20,44 @@ export class BooksComponent {
 
   visible= true;
 
+  counter = 1;
+
   constructor(private bookService: BookService, http: Http,
               private el: ElementRef) {
     this.refreshBooks();
     // http.get('/books.json').subscribe(response => this.books = response.json() || []);
-    this.createObservable();
+    // this.createLatinCharacters();
+    // this.createPrimeNumbers();
   }
 
-  private createObservable() {
+  private createLatinCharacters() {
     Observable.range(0, 25).map(x => String.fromCharCode(x + 'a'.charCodeAt(0)))
       .subscribe(x => console.log(x));
+  }
+
+  private async generate(): Promise<number> {
+     let valid = true;
+     for(let i = this.counter; i < 1000000; i++) {
+       for(let j = 2; j < this.counter; j++) {
+          if(i % j === 0) {
+              valid = false;
+              break;
+          }
+       }
+       if(valid) {
+         return Promise.resolve(this.counter);
+       } else {
+          this.counter++;
+       }
+       valid = true;
+     }
+     return null;
+  }
+
+  private createPrimeNumbers() {
+    // Observable.create((observer: Observer<number>) => {
+    //     const x = await this.generate();
+    // }).take(10).subscribe( x => console.log(x));
   }
 
   refreshBooks(): void {
