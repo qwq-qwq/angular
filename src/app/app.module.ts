f81edc021db72d6327ccd5819b48c1e4ed9f6a95
IT-Discovery <sergey.morenets@gmail.com>
2017-06-10 16:17:49 +0300

    Moved from template-driven way to code-driven way with book registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -30,7 +30,8 @@ import { ShowDirective } from './show.directive';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [BookService, CurrencyPipe],
   bootstrap: [AppComponent]
--- src/app/book-registration/book-registration.component.ts
+++ src/app/book-registration/book-registration.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {BookService} from "../book.service";
-import {Book} from "../book/book";
+import {BookService} from '../book.service';
+import {Book} from '../book/book';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-book-registration',
@@ -8,12 +9,26 @@ import {Book} from "../book/book";
   styleUrls: ['./book-registration.component.css']
 })
 export class BookRegistrationComponent implements OnInit {
+  private bookForm: FormGroup;
 
-  constructor(private bookService: BookService) {
+  constructor(private bookService: BookService, formBuilder: FormBuilder) {
+    this.bookForm = formBuilder.group({
+      title: formBuilder.control('', [Validators.required,
+        Validators.minLength(3)]),
+      author: formBuilder.control('', [Validators.required, Validators.minLength(4)]),
+      year: formBuilder.control('', [Validators.pattern('\\d{4}')]),
+      pages: formBuilder.control(''),
+      description: formBuilder.control('')
+    });
   }
 
-  createBook(book: any): void {
+  isNotValid(controlName: string): boolean {
+    return this.bookForm.get(controlName).touched &&
+      !this.bookForm.get(controlName).valid;
+  }
 
+  createBook(): void {
+    const book: any = this.bookForm.value;
     this.bookService.saveBook(new Book(
       book.title, book.author, book.year, book.description, book.pages, 0));
   }
