ce6b1ad129d8fb9794c8c3426837755a4454857a
IT-Discovery <sergey.morenets@gmail.com>
2017-06-11 15:26:32 +0300

    Adding login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,13 @@ import {Routes} from '@angular/router';
 import {NewsComponent} from './news/news/news.component';
 import {BooksComponent} from './books/books/books.component';
 import {AdminComponent} from "./administration/admin/admin.component";
+import {LoginComponent} from "./login/login.component";
 /**
  * Created by IT-Discovery on 11.06.2017.
  */
 export const ROUTES: Routes = [
-  {
-    'path': '', 'component': NewsComponent
-  },
-  {
-    'path': 'books', 'component': BooksComponent
-  },
-  {
-    path: 'admin', component: AdminComponent
-  }
+  {path: '', component: NewsComponent},
+  {path: 'books', component: BooksComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'login', component: LoginComponent},
 ];
